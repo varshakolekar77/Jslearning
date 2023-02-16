9174f19be00dbf18d6702c6914b11ba2250a78c4
varshakolekar77 <varshakolekar77@gmail.com>
2023-02-16 22:03:11 +0530

    updated the file 01_variable_assign
    updated the file 01_variable_assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,21 @@
+console.log("*************Step 1*********");
+var myFullName;
+console.log(myFullName);
+myFullName="varsha kolekar";
+console.log(myFullName);
+myFullName="varsha balasaheb kolekar";
+console.log(myFullName);
+
+console.log("***********Step 2************");
+var myLove="I love only JS";
+console.log(myLove);
+
+console.log("*******************Step 3*************");
+var firstName="varsha";
+var lastName="kolekar";
+var age="24";
+var address="malegaon bk,tal:baramati,Dist:pune";
+var pincode="413102";
+var gender="female";
+var isMarried="yes";
+console.log("firstName:varsha","lastName:kolekar","age:24","address:malegaon bk,tal:baramati,Dist:pune","gender:female","isMarried:yes");
