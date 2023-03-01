55dee5649e06dc9ab264cd32f18ba596075e8692
varshakolekar77 <varshakolekar77@gmail.com>
2023-03-01 12:03:31 +0530

    update the loop file
    update the loop file

---
+++ 06_for_loop.js
@@ -0,0 +1,29 @@
+//print number 1 to 10
+//initialization 1
+//updateExpression +1
+//condition <=10
+
+for (let index = 1; index <=10; index=index+1) {
+    console.log(index);
+    
+}
+console.log(`Reverse number`);
+for (let index = 10; index >=0; index=index-1) {
+    console.log(index);
+    
+}
+console.log(`Table of 5`);
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);
+    
+}
+console.log(`infinite loop`);
+for (let index = 1; index <10;) {
+    console.log(index);
+    
+}
+console.log(`=================`);
+for (let index = 10; index >=0; index++) {
+    console.log(index);
+    
+}
